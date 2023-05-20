dab492a8374ed2fc3cd3a412e934b53fd5bde61d
xxxsjan <626653354@qq.com>
2023-05-20 15:19:34 +0800

    api

---
+++ src/api/http.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const request = axios.create({
+  baseURL: "https://pcapi-xiaotuxian-front-devtest.itheima.net",
+});
+type HttpOptions = {
+  method: "POST" | "GET" | "PUT" | "DELETE";
+  url: string;
+  data?: any;
+};
+
+export function http<T>(options: HttpOptions) {
+  return request[options.method.toLocaleLowerCase()](options.url, options.data);
+}
